5e9bdf19be39827865ecd623478902a3132477f4
Diane DECOURT <ddecourt@e1r11p16.42.fr>
2021-07-06 17:46:21 +0200

    solving issues & creation of a new alo

--- srcs/selection_sort.c
+++ srcs/selection_sort.c
@@ -113,7 +113,7 @@ int				selection_sort_b(t_stack *a, t_stack *b, int size)
 	return(0);
 }
 
-void 	split_stack(t_stack *a, t_stack *b, int size)
+void 	split_stack_a(t_stack *a, t_stack *b, int size)
 {
 	t_unit *current;
 		int pivot;
@@ -138,13 +138,134 @@ void 	split_stack(t_stack *a, t_stack *b, int size)
 		}
 }
 
+void 	split_stack_b(t_stack *a, t_stack *b, int size)
+{
+	t_unit *current;
+		int pivot;
+		int i;
+
+		current = b->head;
+		pivot = get_mediane(b, size);
+		while (current && !is_divided_b(b, pivot))
+		{
+				i = 0;
+				if (current->data > pivot)
+				{
+						current = current->next;
+						pa(a, b);
+				}
+				if (current->data <= pivot)
+				{
+						rrb(a, b);
+						i++;
+						current = b->head;
+				}
+		}
+}
+
+t_stack	*puts_chunck_in_a(t_stack *a, t_stack *b, int size)
+{
+	while (--size > 0)
+		pa(a, b);
+	return (a);
+}
+
+int		find_smallest_position(t_stack *stack)
+{
+	int value;
+	int position;
+	int i;
+	t_unit *current;
+
+	current = stack->head;
+	value = current->data;
+	position = 0;
+	i = 0;
+	while (current != NULL)
+	{
+		if (current->data < value)
+		{
+			value = current->data;
+			position = i;
+		}
+		i++;
+		current = current->next;
+	}
+	return(position);
+}
+
+int		find_biggest_position(t_stack *stack)
+{
+	int value;
+	int position;
+	int i;
+	t_unit *current;
+
+	current = stack->head;
+	value = current->data;
+	position = 0;
+	i = 0;
+	while (current != NULL)
+	{
+		if (current->data > value)
+		{
+			value = current->data;
+			position = i;
+		}
+		i++;
+		current = current->next;
+	}
+	return(position);
+}
+
+int sort_big_b(t_stack *a, t_stack *b, int position, int size)
+{
+	if (position == 1)
+	{
+		sb(b);
+		pa(a, b);
+		ra(a, b);
+		return(0);
+	}
+	if (position > size/2)
+	{
+		while (--position > -1)
+			rb(a, b);
+		pa(a, b);
+		ra(a, b);
+		return(0);
+	}
+	if (position < size/2)
+	{
+		while (--position > -1)
+			rrb(a, b);
+		pa(a, b);
+		ra(a, b);
+		return (0);
+	}
+	return (1);
+}
+int	sort_chunk_in_b(t_stack *a, t_stack *b, int size)
+{
+	int pos_biggest;
+	int pos_smallest;
+
+	pos_biggest = find_biggest_position(b);
+	printf("BIGGEST POSITION -> %i\n", pos_biggest);
+	pos_smallest = find_smallest_position(b);
+	sort_big_b(a, b, pos_biggest, size/2);
+	/*sort_small_b(a, b, pos_smallest, size/2);*/
+	return (0);
+}
+
 int		better_selection_sort(t_stack *a, t_stack *b, int size)
 {
-
-	split_stack(a, b, size);
-	/*selection_sort(b, a, get_stack_size(b));*/
-	selection_sort_b(a, b, get_stack_size(b));
-	selection_sort_a(a, b, get_stack_size(a));
+	split_stack_a(a, b, size);
+	printf("STACK A\n");
+	read_stack(a);
+	printf("\nSTACK B\n");
+	read_stack(b);
+	sort_chunk_in_b(a, b, get_stack_size(b));
 
 	return(0);
 }

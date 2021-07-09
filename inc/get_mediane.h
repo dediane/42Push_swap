c8d8d6da6745b52500d0df8b82335e7d9403d735
Diane DECOURT <ddecourt@e1r12p4.42.fr>
2021-07-09 16:49:45 +0200

    adding inc forgotten

---
+++ inc/get_mediane.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_mediane.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/09 16:10:28 by ddecourt          #+#    #+#             */
+/*   Updated: 2021/07/09 16:10:59 by ddecourt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_MEDIANE_H
+# define GET_MEDIANE_H
+
+int	get_mediane(t_stack *stack, int size);
+int	is_divided_a(t_stack *stack, int pivot);
+int	is_divided_b(t_stack *stack, int pivot);
+
+#endif
